d81904371ca37a9586fd97903744491578aebd2d
Chris Ancheta <c-ancheta@live.com>
2016-12-13 15:57:09 -0500

    Updated README.md and added Test functionality to file creation section in VariableExample1.sh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-This is a dummy README file.
+This repository is first and foremost utilized as a walk-through guide for Shell Scripting. This repository will contain the following:
+* Example code as to try and centralize my knowledge base as it pertains to Shell Scripting
+* A sandbox as to explore the various capabilities that can be utilized with Shell Scripting
+*
--- VariablesExample1.sh
+++ VariablesExample1.sh
@@ -2,12 +2,23 @@
 
 # Introduction to variables in Shell Scripting
 
-MY_MESSAGE="Hello World"
-echo -e "$MY_MESSAGE\nI'm interested in knowing you. What is your name?: "
+# MY_MESSAGE="Hello World"
+# echo -e "$MY_MESSAGE\nI'm interested in knowing you. What is your name?: "
 # 'read' is the command to take in user input into script at runtime
-read MY_NAME
-echo "Why, Hello $MY_NAME. Hope all is well"
+#read MY_NAME
+# echo "Why, Hello $MY_NAME. Hope all is well"
 
 #'$(cmd)' allows one to store the value outputted by the command. In this case, cmd
+USER_NAME="caancheta"
 CURRENT_DIR=$(pwd)
 echo "Did you know your pwd is $CURRENT_DIR."
+echo "Creating file: ${USER_NAME}_file.txt"
+echo "test file demo" > ${USER_NAME}_file.txt
+if [[ -n $(find . -name ${USER_NAME}_file.c) ]]
+then
+  echo "${USER_NAME}_file.txt was created"
+  echo "Deleting file: ${USER_NAME}_file.txt"
+  rm ${USER_NAME}_file.txt
+else
+  echo "Error when creating: ${USER_NAME}_file.txt"
+fi
